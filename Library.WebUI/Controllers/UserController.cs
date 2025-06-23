94552ee89638b3f645690a45ae98896fc03f9258
Batuhan Kocagöz <149085191+batuhankocagoz@users.noreply.github.com>
2025-06-23 21:27:51 +0300

    updated

---
+++ Library.WebUI/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿using Library.WebUI.Dtos.UserDto;
+using Library.WebUI.Services.RoleServices;
+using Library.WebUI.Services.UserServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.WebUI.Controllers;
+
+
+public class UserController : Controller
+{
+    private readonly IUserService _userService;
+    private readonly IHttpContextAccessor _contextAccessor;
+
+    public UserController(IUserService userService,
+                          IHttpContextAccessor contextAccessor)
+    {
+        _userService = userService;
+        _contextAccessor = contextAccessor;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllUserList()
+    {
+        var accessToken = _contextAccessor.HttpContext.Session.GetString("token");
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        var roles = GetUserRoles.GetRolesFromToken(accessToken);
+        if (!roles.Contains("admin") && !roles.Contains("manager"))
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        var response = await _userService.GetUserListAsync();
+        var userList = response.Data ?? new List<UserDto>(); 
+        return View(userList);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUserById(string id)
+    {
+        var accessToken = _contextAccessor.HttpContext.Session.GetString("token");
+        if (string.IsNullOrEmpty(accessToken))
+        {
+            return RedirectToAction("Login", "Account");
+        }
+        var roles = GetUserRoles.GetRolesFromToken(accessToken);
+        if (!roles.Contains("admin") && !roles.Contains("manager"))
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        var response = await _userService.GetUserByIdAsync(id);
+        return View(response.Data);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ApproveUser(string userId)
+    {
+        await _userService.ApproveUserAsync(userId);
+        return RedirectToAction("GetAllUserList");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AssignToManagerRole(string userId)
+    {
+        await _userService.AssignToManagerRoleAsync(userId);
+        return RedirectToAction("GetAllUserList");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AssignToAdminRole(string userId)
+    {
+        await _userService.AssignToAdminRoleAsync(userId);
+        return RedirectToAction("GetAllUserList");
+    }
+}
